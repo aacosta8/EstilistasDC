cc0f74a2fb997317ff04de696b1f2a1ea22d8fc1
Alex <aacosta@eafit.edu.co>
2016-10-01 17:06:53 -0500

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+class WelcomeController < ApplicationController
+  def index
+  	@Holamundo = "Hola Estilistas"
+  end
+  def main
+  end
+end
---
+++ public/app.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name pi1App
+ * @description
+ * # pi1App
+ *
+ * Main module of the application.
+ */
+angular.module('pi1App', ['ui.router'])
+.config(function ($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: '/main.html',
+    controller: 'welcome',
+    controllerAs: 'welcome'
+  })
+  .when('/sobreNosotros', {
+    templateUrl: 'views/sobreNosotros.html',
+    controller: 'SobreNosotrosCtrl',
+    controllerAs: 'sobreNosotros'
+  })
+  .when('/contact', {
+    templateUrl: 'views/contact.html',
+    controller: 'ContactCtrl',
+    controllerAs: 'contact'
+  })
+  .when('/login', {
+    templateUrl: 'views/login.html',
+    controller: 'LoginCtrl',
+    controllerAs: 'login'
+  })
+  .when('/testimonios', {
+    templateUrl: 'views/testimonios.html',
+    controller: 'TestimoniosCtrl',
+    controllerAs: 'testimonios'
+  })
+  .when('/noticias', {
+    templateUrl: 'views/noticias.html',
+    controller: 'NoticiasCtrl',
+    controllerAs: 'noticias'
+  })
+  .when('/registro', {
+    templateUrl: 'views/registro.html',
+    controller: 'RegistroCtrl',
+    controllerAs: 'registro'
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
+});
