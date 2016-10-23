bf8f6aaa579b47b86ee088540a300239a3c7b241
Alex <aacosta@eafit.edu.co>
2016-10-22 19:55:18 -0500

    Registro de clientes y estilistas y el login sin alerts

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,50 +1,96 @@
 class WelcomeController < ApplicationController
+
   def index
-  	# @Holamundo = "Hola Estilistas"
   end
+
   def main
   end
+
   def contact
   end
+
   def login
   end
+
   def noticias
   end
+
   def registro
   end
+
   def sobreNosotros
   end
+
   def testimonios
   end
+
   def entrar
-  	parametros_seguros = entrar_params
-  	correo = parametros_seguros[:correo]
-    clave = parametros_seguros[:clave]
-    estilista = Stylist.find_by(correo_electronico: correo)
-    # cliente = Customer.find_by(correo_electronico: correo)
-    if estilista
-     # render plain: "Hola"
-     redirect_to controller: 'estilistas', action: 'mainest'
+
+    parametros_seguros = entrar_params
+    cedula = parametros_seguros[:cedula]
+    nombre = parametros_seguros[:nombre]
+
+    if persona = Person.find_by(cedula: cedula)
+
+      if persona.tipo == "Cliente" and persona.nombre == nombre
+       redirect_to controller: 'clientes', action: 'mainC'
+
+     else
+
+
+
+
+     end
+
+
+     if persona.tipo == "Estilista" and persona.nombre == nombre
+       redirect_to controller: 'clientes', action: 'mainC'
+
+     else
+
+     end
    else
-     redirect_to controller: 'clientes', action: 'mainC'
-     #render plain: "No"
-   end
- end
- def registrar
-  render plain: "Clic en registrar"
- #  	#Despues de lo explicado para entrar
- #  	persona = Persona.new
- #  	persona.nombre = nombre;
- #  	#.
- #  	#.
- #  	#.
- #  	unless persona.save
- #  		aler
-	# end
+    flash[:notice] = "Campos vacíos"
+  end
 end
 
+def registrarme
+end
+
+
+def regclientes
+
+  person = Person.new
+  datos = regcliente_params
+
+  person.nombre = datos[:nombre]
+  person.apellido = datos[:apellido]
+  person.cedula = datos[:cedula]
+  person.fecha_nacimiento = datos[:nacimiento]
+  person.genero = datos[:genero]
+  person.telefono_movil = datos[:movil]
+  person.telefono_fijo = datos[:fijo]
+  person.tipo = "Cliente"
+  person.ubication_id = 1
+
+  person.save
+
+  cliente = Customer.new
+  cliente.person_id = person.id
+  cliente.tipo_discapacidad =datos[:tipo] 
+
+  cliente.save
+end
+
+
 private
+
 def entrar_params
- params.permit(:correo,:clave)
+ params.permit(:cedula,:nombre)
+end
+
+def regcliente_params
+ params.permit(:nombre, :apellido, :cedula, :nacimiento, :genero, :movil, :fijo, :tipo)
 end
 end
+
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,5 @@
 class Person < ApplicationRecord
   belongs_to :ubication
+  validates :cedula, uniqueness: true
+
 end
